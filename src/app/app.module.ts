bc95fdb4dbc6463f9ef69bf018d714d8b6701eb7
randall5559 <randallakers.work@gmail.com>
2017-03-30 14:13:45 -0400

    * Setup job state model with data and action services * Create mock response for test * Wrote test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {
+  StateManagerService,
+  DataService,
+  ActionsService
+ } from './shared/services';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +23,25 @@ import { HeaderComponent } from './header/header.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    StateManagerService,
+    DataService,
+    ActionsService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  /**
+   * Creates an instance of AppModule.
+   * 
+   * @param {StateManagerService} stateManager
+   * 
+   * @memberOf AppModule
+   */
+  constructor(private stateManager: StateManagerService) {
+    /** Job Scheduler Model */
+    stateManager.createModel('JobScheduler');
+    stateManager.setModel('JobScheduler', {});
+  }
+}
---
+++ src/app/shared/services/data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class DataService {
+
+  constructor(
+    private Http: Http) { }
+
+
+  /**
+   *
+   * 
+   * @returns {Observable<any>}
+   * 
+   * @memberOf DataService
+   */
+  public getJobsData(): Observable<any> {
+    let url: string = '../../../assets/jobs.json';
+
+    return this.Http.get(url)
+      .map((response: Response) => {
+        return response.json();
+      });
+  }
+
+}
